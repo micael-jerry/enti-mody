bc01b36515d8f0e10b226ba6765cb307bfd4b38e
Micael Jerry <fidimalala.mj@gmail.com>
2025-11-04 15:32:55 +0300

    chore: reactive breadcumb

--- src/app/layout/breadcrumb/breadcrumb.component.ts
+++ src/app/layout/breadcrumb/breadcrumb.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 
 @Component({
 	selector: 'app-breadcrumb',
@@ -7,5 +7,6 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 	templateUrl: './breadcrumb.component.html',
 })
 export class BreadcrumbComponent {
-	title = 'login/Register';
+	@Input({ required: false })
+	value: string | null = null;
 }
