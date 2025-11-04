d0615838baa23eea8b48ac4fdf205b3b107ba9a2
Micael Jerry <fidimalala.mj@gmail.com>
2025-11-04 15:32:55 +0300

    chore: reactive breadcumb

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { BreadcrumbComponent } from '../layout/breadcrumb/breadcrumb.component';
 
 @Component({
 	selector: 'app-home',
 	changeDetection: ChangeDetectionStrategy.OnPush,
 	standalone: true,
 	templateUrl: './home.component.html',
+	imports: [BreadcrumbComponent],
 })
 export class HomeComponent {}
