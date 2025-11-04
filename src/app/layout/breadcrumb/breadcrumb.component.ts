6700cb99eebd20b5bf36daf9cff1a75e1b6dd538
Micael Jerry <fidimalala.mj@gmail.com>
2025-11-04 17:03:00 +0300

    chore: update breadcumb

--- src/app/layout/breadcrumb/breadcrumb.component.ts
+++ src/app/layout/breadcrumb/breadcrumb.component.ts
@@ -1,13 +1,19 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
 	selector: 'app-breadcrumb',
 	standalone: true,
 	changeDetection: ChangeDetectionStrategy.OnPush,
+	imports: [RouterLink],
 	templateUrl: './breadcrumb.component.html',
+	styleUrl: './breadcrumb.component.css',
 })
 export class BreadcrumbComponent {
 	private readonly router = inject(Router);
-	value: string = this.router.url;
+	pathArray: string[] = this.getPathArray(this.router.url);
+
+	private getPathArray(url: string): string[] {
+		return url.split('/').filter((path) => path.length > 0);
+	}
 }
