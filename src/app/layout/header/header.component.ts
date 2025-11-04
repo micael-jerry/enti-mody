6e430c7eb5199844cfac102c62e9afeba1f257ef
Micael Jerry <fidimalala.mj@gmail.com>
2025-11-04 20:22:43 +0300

    chore: update header

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,9 +1,12 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
 	selector: 'app-header',
 	standalone: true,
 	changeDetection: ChangeDetectionStrategy.OnPush,
 	templateUrl: './header.component.html',
+ imports: [RouterLink, NgOptimizedImage],
 })
 export class HeaderComponent {}
