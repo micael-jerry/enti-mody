c22643a77d10838a3bb7619bf7e68c9fdea0e1f7
Micael Jerry <fidimalala.mj@gmail.com>
2025-11-05 08:25:53 +0300

    fix: subscribe breadcumb to router events

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,12 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { BreadcrumbComponent } from '../layout/breadcrumb/breadcrumb.component';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
 	selector: 'app-auth',
 	standalone: true,
 	changeDetection: ChangeDetectionStrategy.OnPush,
-	imports: [BreadcrumbComponent, RouterLink, RouterOutlet, RouterLinkActive],
+	imports: [RouterLink, RouterOutlet, RouterLinkActive],
 	templateUrl: './auth.component.html',
 	styleUrl: './auth.component.css',
 })
