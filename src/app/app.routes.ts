e4630ecb062efb981294f840d0d7e6e090a49e37
Micael Jerry <fidimalala.mj@gmail.com>
2025-11-05 06:08:02 +0300

    fix: home router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,15 +4,14 @@ import { AuthLoginComponent } from './auth/login/auth-login.component';
 import { AuthComponent } from './auth/auth.component';
 
 export const routes: Routes = [
-	{ path: '', redirectTo: 'home', pathMatch: 'full' },
-	{ path: 'home', component: HomeComponent },
 	{
 		path: 'auth',
 		component: AuthComponent,
-		pathMatch: 'full',
 		children: [
-			{ path: '', redirectTo: 'login', pathMatch: 'full' },
 			{ path: 'login', component: AuthLoginComponent, pathMatch: 'full' },
+			{ path: '', redirectTo: 'login', pathMatch: 'full' },
 		],
 	},
+	{ path: 'home', component: HomeComponent },
+	{ path: '', redirectTo: 'home', pathMatch: 'full' },
 ];
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,12 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { BreadcrumbComponent } from '../layout/breadcrumb/breadcrumb.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
 	selector: 'app-home',
 	changeDetection: ChangeDetectionStrategy.OnPush,
 	standalone: true,
 	templateUrl: './home.component.html',
-	imports: [BreadcrumbComponent],
+	imports: [BreadcrumbComponent, RouterLink],
 })
 export class HomeComponent {}
