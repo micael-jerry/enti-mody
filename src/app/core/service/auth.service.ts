c0eb82ae5d335e2a828cf13d8f74b2e8ecec5bc7
Micael Jerry <fidimalala.mj@gmail.com>
2025-11-06 06:34:19 +0300

    chore: save auth token on local storage

--- src/app/core/service/auth.service.ts
+++ src/app/core/service/auth.service.ts
@@ -3,16 +3,24 @@ import { inject, Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment';
 import { AuthLoginDto, AuthLoginResponse } from '../dto/login.dto';
 import { lastValueFrom, Observable } from 'rxjs';
+import { LocalStorageUtil } from '../../shared/utils/local-storage.util';
+import { AUTH_TOKEN_KEY } from '../../shared/constants';
 
 @Injectable()
 export class AuthService {
 	private http = inject(HttpClient);
 
-	login(credentials: AuthLoginDto): Promise<AuthLoginResponse> {
+	async login(credentials: AuthLoginDto): Promise<AuthLoginResponse> {
 		const loginResponse$: Observable<AuthLoginResponse> = this.http.post<AuthLoginResponse>(
 			`${environment.apiUrl}/login`,
 			credentials,
 		);
-		return lastValueFrom(loginResponse$);
+		const loginResponse: AuthLoginResponse = await lastValueFrom(loginResponse$);
+		LocalStorageUtil.setItem(AUTH_TOKEN_KEY, loginResponse.token);
+		return loginResponse;
+	}
+
+	isAuthenticated(): boolean {
+		return LocalStorageUtil.getItem(AUTH_TOKEN_KEY) !== null;
 	}
 }
