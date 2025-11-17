847318f272e4acbe577ba3de3e0a799743e85b82
Miael Jerry <jerry@capsule.mg>
2025-11-17 15:01:20 +0300

    chore: user list stupid component

--- src/app/features/user/user-list/user-list.component.ts
+++ src/app/features/user/user-list/user-list.component.ts
@@ -1,16 +1,6 @@
-import {
-	ChangeDetectionStrategy,
-	Component,
-	computed,
-	input,
-	linkedSignal,
-	Signal,
-	signal,
-	WritableSignal,
-} from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, input, output } from '@angular/core';
 import { User } from '../../../core/model/user.model';
 import { NgOptimizedImage } from '@angular/common';
-import { USER_LIST_PAGE_SIZE } from '../../../core/constants/user.constants';
 import { formatUserRoles } from '../../../shared/utils/user.util';
 
 @Component({
@@ -21,46 +11,26 @@ import { formatUserRoles } from '../../../shared/utils/user.util';
 	imports: [NgOptimizedImage],
 })
 export class UserListComponent {
-	readonly fetchedUsers = input<User[]>([]);
+	readonly users = input.required<User[]>();
+	readonly currentPage = input.required<number>();
+	readonly totalPages = input.required<number>();
 
-	readonly formatUserRoles = formatUserRoles;
+	readonly newPage = output<number>();
+	readonly searchQuery = output<string>();
 
-	readonly searchQuery: WritableSignal<string> = signal('');
-	readonly users: WritableSignal<User[]> = linkedSignal(() => this.fetchedUsers());
-
-	readonly currentPage = signal(1);
-	readonly totalPages = computed(() => Math.max(1, Math.ceil(this.users().length / USER_LIST_PAGE_SIZE)));
 	readonly pageNumbers = computed(() =>
 		Array(this.totalPages())
 			.fill(0)
 			.map((_, i) => i + 1),
 	);
-	readonly usersOnCurrentPage: Signal<User[]> = computed(() => {
-		const start = (this.currentPage() - 1) * USER_LIST_PAGE_SIZE;
-		return this.users().slice(start, start + USER_LIST_PAGE_SIZE);
-	});
+
+	onPageChange(pageNumber: number): void {
+		this.newPage.emit(pageNumber);
+	}
 
 	onSearchQueryInput(searchQuery: string): void {
-		this.searchQuery.set(searchQuery);
-		this.currentPage.set(1);
-
-		const queryIgnoreCase = searchQuery.toLocaleLowerCase();
-		const filteredUsers = this.fetchedUsers().filter((user) =>
-			(user.name + user.email).toLocaleLowerCase().includes(queryIgnoreCase),
-		);
-		this.users.set(filteredUsers);
+		this.searchQuery.emit(searchQuery);
 	}
 
-	set page(page: number) {
-		const p = Math.max(1, Math.min(this.totalPages(), page));
-		this.currentPage.set(p);
-	}
-
-	nextPage(): void {
-		this.page = this.currentPage() + 1;
-	}
-
-	prevPage(): void {
-		this.page = this.currentPage() - 1;
-	}
+	protected readonly formatUserRoles = formatUserRoles;
 }
